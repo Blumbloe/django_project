ff9db9a024d464e87aa1f0abae79878b01f3dfab
Blumbloe <c05w@icloud.com>
2026-03-15 22:14:03 +0000

    Change code structure of ALLOWED_HOSTS

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = 'django-insecure-%kz+6(lnbe_#-zq^+157fhr895egc2*09(y#vod-k)l)+hw5v7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '.herokuapp.com',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1',]
 
 
 # Application definition
